c92d7a9cfa2a730e5d07a517ab3c10634ac7c341
Dayvin Humberto Perez Castillo <dayvin.perez@uhispano.ac.cr>
2022-01-11 22:50:57 -0600

    Lab02

--- MobileApp/MobileApp/App.cs
+++ MobileApp/MobileApp/App.cs
@@ -3,11 +3,44 @@ using Xamarin.Forms;
 
 namespace MobileApp
 {
-	public partial class App : Application
+    public partial class App : Application
 	{
 		public App()
 		{
-			MainPage = new ContentPage { Content = new Label { Text = "Hello, World!", VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Center } };
+			Button openHello = new Button { Text = "Go to Hello" };
+			var openControls = new Button { Text = "Go To Controls Dayvin" };
+
+
+			var page = new ContentPage
+			{
+				Content = new StackLayout
+				{
+					Orientation = StackOrientation.Vertical,
+					Children =
+					{
+					openHello,
+					openControls,
+					}
+				}
+
+			};
+
+			openHello.Command = new Command(() => page.Navigation.PushAsync(new HelloWorldPage()));
+			openControls.Command = new Command(() => page.Navigation.PushAsync(new  Views.ControlsPage()));
+
+
+			View titleView = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Children =
+				{
+					new Label {Text = "Welcome Dayvin" }
+				}
+			};
+			NavigationPage.SetTitleView(page, titleView);
+
+			MainPage = new NavigationPage(page);
 		}
 	}
 }
